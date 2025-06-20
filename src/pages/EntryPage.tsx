bf2d75dd74d7486477b21bf5327f0f0a6743a295
sid-darth-73 <siddharthraj532@gmail.com>
2025-06-20 13:24:26 +0530

    minor changes

--- src/pages/EntryPage.tsx
+++ src/pages/EntryPage.tsx
@@ -2,7 +2,7 @@ import { useRef, useEffect } from "react";
 import { Button } from "../components/ui/Button";
 import { useNavigate } from "react-router-dom";
 import { Input } from "../components/Input";
-
+import { HeartIcon } from "../components/ui/HeartIcon";
 export function EntryPage() {
     const primaryUserNameRef = useRef<HTMLInputElement>(null);
     const secondaryUserNameRef = useRef<HTMLInputElement>(null);
@@ -25,6 +25,7 @@ export function EntryPage() {
             navigate("/multipleUser");
         }
     }
+
     useEffect(() => {
         function handleKeyPress(e: KeyboardEvent) {
             if (e.key === "Enter") {
@@ -37,19 +38,19 @@ export function EntryPage() {
     }, []);
 
     return (
-        <div className="flex items-center justify-center min-h-screen bg-slate-900 text-white p-4">
-            <div className="bg-slate-800 rounded-xl p-8 w-full max-w-lg shadow-lg space-y-6">
-                <h1 className="text-2xl text-center mb-2 font-mont">Welcome to Compare Forces!</h1>
-                <Input
-                    placeholder="Enter your Codeforces handle"
-                    reference={primaryUserNameRef}
-                />
-                <Input
-                    placeholder="Enter your friend's Codeforces handle(optional)"
-                    reference={secondaryUserNameRef}
-                />
-                <div className="flex items-center justify-center">
-                    <div >
+        <div className="flex flex-col justify-between min-h-screen bg-slate-900 text-white p-4">
+            <div className="flex flex-grow items-center justify-center">
+                <div className="bg-slate-800 rounded-xl p-8 w-full max-w-lg shadow-lg space-y-6">
+                    <h1 className="text-2xl text-center mb-2 font-mont">Welcome to Compare Forces!</h1>
+                    <Input
+                        placeholder="Enter your Codeforces handle"
+                        reference={primaryUserNameRef}
+                    />
+                    <Input
+                        placeholder="Enter your friend's Codeforces handle (optional)"
+                        reference={secondaryUserNameRef}
+                    />
+                    <div className="flex items-center justify-center">
                         <Button
                             variant="primary"
                             text="Click (or press Enter)"
@@ -58,19 +59,15 @@ export function EntryPage() {
                         />
                     </div>
                 </div>
-                
             </div>
-            {/* <div>
-                <div className="font-mono">
-                    Test font family 200
+
+            <footer className=" text-center text-gray-400 text-sm mt-8 mb-2 font-quick ">
+                <div className="flex items-center justify-center">
+                <div> Made with  </div>
+                <div> <HeartIcon/> </div>
+                <div>  by <a href="https://github.com/sid-darth-73/compareForces"> unbit</a></div>
                 </div>
-                <div className="font-mont">
-                    Test font family 200
-                </div>
-                <div>
-                    Test font family 200
-                </div>
-            </div> */}
+            </footer>
         </div>
     );
 }
